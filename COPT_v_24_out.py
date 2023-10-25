21a7294df78925943e213cb05d478c877e6605f3
Eduardo Mendes <eduardo.mendes@engenharia.ufjf.br>
2023-10-25 03:44:32 -0300

    mudanças nos arquivos COPT
    foi feita a comparação entre os arquivos, sendo necessárias modificações em ambos. o do Davi estava com erros na curva, quando mudei e a probabilidade bateu, os índices tbm bateram.

--- COPT_v_24_out.py
+++ COPT_v_24_out.py
@@ -96,10 +96,10 @@ def pu(x):
     return x/100
 
 
-data = pd.read_excel('C:/Users/Davi/OneDrive/Mestrado/Confiabilidade/Trabalho 1/Gerac.xlsx')
+data = pd.read_excel('Gerac.xlsx')
 data['FOR'] = data['FOR'].apply(pu)
 
-curva_de_carga = pd.read_excel('C:/Users/Davi/OneDrive/Mestrado/Confiabilidade/Trabalho 1/curva de carga.xlsx', skiprows=[1,2,3])
+curva_de_carga = pd.read_excel('curva de carga.xlsx', skiprows=[1,2,3])
 curva_de_carga.rename(columns={'Inverno ': 'Inverno sem','Unnamed: 2':'Inverno fds',
                                'Verão': 'Verao sem','Unnamed: 4':'Verao fds',
                                'Primavera/Outono': 'Prim/Out sem','Unnamed: 6':'Prim/Out fds'}, inplace=True)
@@ -111,22 +111,24 @@ valores_de_carga = curva_de_carga.stack()
 valores_de_carga = pd.unique(valores_de_carga)
 valores_de_carga = np.sort(valores_de_carga)
 
+print(curva_de_carga)
+
 def Calcula_Prob(patamar):
-    inver_sem = curva_de_carga[curva_de_carga['Inverno sem'] == patamar]['Inverno sem'].size*7
-    inver_fds = curva_de_carga[curva_de_carga['Inverno fds'] == patamar]['Inverno fds'].size*2
-    verao_sem = curva_de_carga[curva_de_carga['Verao sem'] == patamar]['Verao sem'].size*7
-    verao_fds = curva_de_carga[curva_de_carga['Verao sem'] == patamar]['Verao sem'].size*2
-    priout_sem = curva_de_carga[curva_de_carga['Prim/Out sem'] == patamar]['Prim/Out sem'].size*7
-    priout_fds = curva_de_carga[curva_de_carga['Prim/Out sem'] == patamar]['Prim/Out sem'].size*2
-    return sum([inver_sem, inver_sem, verao_sem, verao_fds, priout_sem, priout_fds])
+    inver_sem = curva_de_carga[curva_de_carga['Inverno sem'] == patamar]['Inverno sem'].size    *5*(8-1+52-44+2)
+    inver_fds = curva_de_carga[curva_de_carga['Inverno fds'] == patamar]['Inverno fds'].size    *2*(8-1+52-44+2)
+    verao_sem = curva_de_carga[curva_de_carga['Verao sem'] == patamar]['Verao sem'].size        *5*(30-18+1)
+    verao_fds = curva_de_carga[curva_de_carga['Verao fds'] == patamar]['Verao fds'].size        *2*(30-18+1)
+    priout_sem = curva_de_carga[curva_de_carga['Prim/Out sem'] == patamar]['Prim/Out sem'].size *5*(17-9+43-31+2)
+    priout_fds = curva_de_carga[curva_de_carga['Prim/Out fds'] == patamar]['Prim/Out fds'].size *2*(17-9+43-31+2)
+    return sum([inver_sem, inver_fds, verao_sem, verao_fds, priout_sem, priout_fds])
     
     
 inv_sem = 5*(8-1+52-44+2)*24
 inv_fds = 2*(8-1+52-44+2)*24
 ver_sem = 5*(30-18+1)*24
 ver_fds = 2*(30-18+1)*24
-po_sem = 5*(17-9+43-31+2)*24
-po_fds = 2*(17-9+43-31+2)*24
+po_sem =  5*(17-9+43-31+2)*24
+po_fds =  2*(17-9+43-31+2)*24
 
 horasano = inv_fds + inv_sem + ver_sem + ver_fds + po_sem + po_fds
 
@@ -134,14 +136,19 @@ EPNS = []
 EENS = []
 LOLP = []
 LOLE = []
+probab = []
 
 for i in valores_de_carga:
     copt = Calcula_COPT(data, carga*i/100)
-    EPNS.append(copt[0]*Calcula_Prob(i)/horasano)
-    EENS.append(copt[1]*Calcula_Prob(i)/horasano)
-    LOLP.append(copt[2]*Calcula_Prob(i)/horasano)
-    LOLE.append(copt[3]*Calcula_Prob(i)/horasano)
+    probab_i = Calcula_Prob(i)/horasano
+    probab.append(probab_i)
+    EPNS.append(copt[0]*probab_i)
+    EENS.append(copt[1]*probab_i)
+    LOLP.append(copt[2]*probab_i)
+    LOLE.append(copt[3]*probab_i)
     
+    # a cada iteração, plotar o valor de carga e a probabilidade
+    print('Carga: {} MW'.format(i), 'Probabilidade: {}'.format(probab_i))
 
 print('A LOLP do problema é: {}'.format(sum(LOLP)))
 print('A EPNS do problema é: {} MW'.format(sum(EPNS)))
